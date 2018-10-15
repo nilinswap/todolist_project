516493de03ccd0098eaa8229970a0ba11c4825f7
nilinswap <nilinswap@gmail.com>
2018-10-15 18:51:20 +0530

    creating todos

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,5 +1,22 @@
 
 //J
-$("li").click(function(){
+$("ul").on("click","li", function(){
 	$(this).toggleClass("completed");
-})
+});
+
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(1000, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+	//$(this).remove();
+
+});
+
+$("input[type='text']").on("keypress", function(event){
+	if( event.which === 13){
+		var tex = $(this).val();
+		$(this).val("");
+		$("ul").append("<li> <span>X</span>  " + tex + "</li>");
+	}
+});
